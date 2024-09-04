87cb5d79120edb1d614856a7fd4417d99f6dc115
Alan Sumiya <alan.sumiya@gmail.com>
2024-09-04 15:53:47 -0300

    Consulta simples com @Query

--- src/main/java/com/alansumiya/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alansumiya/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.alansumiya.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alansumiya.workshopmongo.domain.Post;
@@ -13,4 +14,12 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	//Query methods monta automaticamente uma consulta específica
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//primeiro campo é qual o campo que eu vou efetuar a busca (title)
+	//segundo campo é quem vai ser a expressão regular, que é o valor que é recebido pelo text
+	//para dizer que é o primeiro parâmetro q quero aproveitar uso ?0, se tivesse mais de um e
+	//quisesse aproveitar o segundo coloco ?1
+	//terceiro campo coloco i para ignorar letras maiúsculas
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
